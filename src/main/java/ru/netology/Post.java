8dc9673f7a298a0f08ac7bbe6324461ed072262e
Dmitry Bychkov <yom.4100@gmail.com>
2022-11-12 16:33:41 +0300

    add some code

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Post {
+    FormDate birthday = new FormDate();
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
